600ca2ebe0dd314ab6902eaa36d26644c2786024
Davi-Mota-Nogueira <davidamota.nogueira@gmail.com>
2021-12-08 00:35:03 -0300

    Curso IDE Visual Studio

---
+++ IDE Instalação e Configuração (Visual Studio)/Teste-Gerando-Codigo/Teste-Gerando-Codigo/Aritmetica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aritmetica
+{
+    public class Aritmetica
+    {
+
+        public int Soma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtracao(int x, int y)
+        {
+            return x - y;
+        }
+
+    }
+}
---
+++ IDE Instalação e Configuração (Visual Studio)/Teste-Gerando-Codigo/Teste-Gerando-CodigoTests/AritmeticaTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Aritmetica;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aritmetica.Tests
+{
+    [TestClass()]
+    public class AritmeticaTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            //Arrange
+            var x = 4;
+            var y = 5;
+            var esperado = 9;
+            var aritmetica = new Aritmetica();
+
+            //Act
+            var result = aritmetica.Soma(x, y);
+
+            //Assert
+            Assert.AreEqual(esperado, result);
+        }
+
+        [TestMethod()]
+        public void SubtracaoTest()
+        {
+            //Arrange
+            var x = 10;
+            var y = 5;
+            var esperado = y;
+            var aritca = new Aritmetica(); 
+
+            //Act
+            var result = aritca.Subtracao(x, y);
+
+            //Assert
+            Assert.AreEqual(esperado, result);
+        }
+    }
+}
